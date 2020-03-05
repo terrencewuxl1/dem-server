8b626ed347dd7dd5f1a1ad9190a5858b6c9d57dd
WuTx <WuTx@CalSAWS.org>
2020-03-05 15:18:00 -0800

    log4j2 success

--- src/main/java/com/dem/server/DemApplication.java
+++ src/main/java/com/dem/server/DemApplication.java
@@ -1,13 +1,19 @@
 package com.dem.server;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemApplication {
 
+    private final static Logger LOGGER= LoggerFactory.getLogger(DemApplication.class);
+
     public static void main(String[] args){
+
         SpringApplication.run(DemApplication.class,args);
+        LOGGER.info("Simple log statement with inputs {}, {} and {}", 1,2,3);
     }
 }
