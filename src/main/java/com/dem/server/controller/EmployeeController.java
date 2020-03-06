90f91247b3a9f731f2e16b19320df06e0e1f9c83
WuTx <WuTx@CalSAWS.org>
2020-03-06 14:50:56 -0800

    controller

--- src/main/java/com/dem/server/controller/EmployeeController.java
+++ src/main/java/com/dem/server/controller/EmployeeController.java
@@ -1,11 +1,43 @@
 package com.dem.server.controller;
 
+import com.dem.server.controller.bean.Employee;
+import com.dem.server.controller.bean.Employees;
+import com.dem.server.controller.dao.EmployeeDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
-@RequestMapping(path="/employees")
+@RequestMapping(path = "/employees")
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    public Employees getEmployees() {
+        return employeeDao.getAllEmployees();
+    }
+
+    @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addEmployee(@RequestBody Employee employee) {
+        Integer id = employeeDao.getAllEmployees().getEmployeeList().size() + 1;
+        employee.setId(id);
+
+        employeeDao.addEmployee(employee);
+
+        URI location =
+                ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("{/id}")
+                        .buildAndExpand(employee.getId())
+                        .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
 
 }
--- src/main/java/com/dem/server/controller/bean/Employees.java
+++ src/main/java/com/dem/server/controller/bean/Employees.java
@@ -1,15 +1,15 @@
 package com.dem.server.controller.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class Employees {
 
     private List<Employee> employeeList;
-
-    public void setEmployeeList(List<Employee> employeeList) {
-        this.employeeList = employeeList;
-    }
 
     public List<Employee> getEmployeeList() {
         return this.employeeList == null ? new ArrayList<>() : employeeList;
